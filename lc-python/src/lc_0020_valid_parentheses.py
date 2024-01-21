cbe34d5f2bce05e0566f774c961af5db8a8e8836
Miltiadis Orfanos <morfanos@tuta.io>
2024-01-21 13:51:20 -0500

    solves 20 valid parentheses (python)

---
+++ lc-python/src/lc_0020_valid_parentheses.py
@@ -0,0 +1,17 @@
+def isValid(s: str) -> bool:
+    """
+    Keep track of the opening braces in a list/stack. When a closing brace is
+    found, check against the most recent opening brace.
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
+    tokens = {"(": ")", "{": "}", "[": "]"}
+    stack = []
+    for ch in s:
+        if ch in "({[":
+            stack.append(ch)
+        else:
+            if len(stack) == 0 or tokens[stack.pop()] != ch:
+                return False
+    return len(stack) == 0
