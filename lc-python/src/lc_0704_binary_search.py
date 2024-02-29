dad4c677a33d78a76671908f008b79530ca6140c
Miltiadis Orfanos <morfanos@tuta.io>
2024-02-29 00:54:39 -0500

    solves 0704 binary search (python)

---
+++ lc-python/src/lc_0704_binary_search.py
@@ -0,0 +1,29 @@
+import unittest
+from typing import List
+
+
+def search(nums: List[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] < target:
+            low = mid + 1
+        elif nums[mid] > target:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+
+class TestBinarySearch(unittest.TestCase):
+    def test_binary_search(self):
+        test_cases = [[[-1, 0, 3, 5, 9, 12], 9, 4], [[-1, 0, 3, 5, 9, 12], 2, -1]]
+        for i, tc in enumerate(test_cases):
+            actual = search(tc[0], tc[1])
+            expected = tc[2]
+            self.assertEqual(actual, expected, f"{i} {tc}")
+
+
+if __name__ == "__main__":
+    unittest.main()
