4a699fd8d9aaf3a191d42bc3153a1f5096a8419e
Miltiadis Orfanos <morfanos@tuta.io>
2024-03-01 10:55:55 -0500

    solves 0733 flood fill (python)

---
+++ lc-python/src/lc_0733_flood_fill.py
@@ -0,0 +1,52 @@
+import unittest
+from typing import List
+
+
+def flood_fill(
+    image: List[List[int]], row: int, col: int, color: int
+) -> List[List[int]]:
+    image_copy = [xs[:] for xs in image]
+
+    color_old = image_copy[row][col]
+    to_fill = [(row, col)]
+
+    while len(to_fill) > 0:
+        (x, y) = to_fill.pop()
+
+        if image_copy[x][y] != color_old or image_copy[x][y] == color:
+            continue
+
+        image_copy[x][y] = color
+
+        if x > 0:
+            to_fill.append((x - 1, y))
+        if x < len(image_copy) - 1:
+            to_fill.append((x + 1, y))
+        if y > 0:
+            to_fill.append((x, y - 1))
+        if y < len(image_copy[x]) - 1:
+            to_fill.append((x, y + 1))
+
+    return image_copy
+
+
+class TestFloodFill(unittest.TestCase):
+    def test_flood_fill(self):
+        test_cases = [
+            [
+                [[1, 1, 1], [1, 1, 0], [1, 0, 1]],
+                1,
+                1,
+                2,
+                [[2, 2, 2], [2, 2, 0], [2, 0, 1]],
+            ],
+            [[[0, 0, 0], [0, 0, 0]], 0, 0, 0, [[0, 0, 0], [0, 0, 0]]],
+        ]
+        for i, tc in enumerate(test_cases):
+            actual = flood_fill(tc[0], tc[1], tc[2], tc[3])
+            expected = tc[4]
+            self.assertEqual(actual, expected, f"{i} {tc}")
+
+
+if __name__ == "__main__":
+    unittest.main()
