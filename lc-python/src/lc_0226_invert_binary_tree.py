376c79f10f7583f05d0a3669d6dae5b9dc2c1a8f
Miltiadis Orfanos <morfanos@tuta.io>
2024-04-13 17:08:13 -0400

    0226 adds solutions

--- lc-python/src/lc_0226_invert_binary_tree.py
+++ lc-python/src/lc_0226_invert_binary_tree.py
@@ -4,10 +4,10 @@ from typing import Optional
 from shared.tree import TreeNode, list_tree_to_tree, tree_to_list
 
 
-def invert_tree(root: Optional[TreeNode]) -> Optional[TreeNode]:
+def invert_tree0(root: Optional[TreeNode]) -> Optional[TreeNode]:
     """
     Time complexity: O(n)
-    Space complexity: O(1)
+    Space complexity: O(n)
     """
     # traverse the tree using depth-first traversal
     # when visiting a node, swap its leaves
@@ -22,23 +22,86 @@ def invert_tree(root: Optional[TreeNode]) -> Optional[TreeNode]:
     root.right = temp
 
     # depth-first traversal
-    invert_tree(root.left)
-    invert_tree(root.right)
+    invert_tree0(root.left)
+    invert_tree0(root.right)
+
+    return root
+
+
+def invert_tree1(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    """
+    Time complexity: O(n)
+    Space complexity: O(n)
+    """
+    s = [root]
+    while s:
+        n = s.pop()
+        if n is None:
+            continue
+
+        # swap
+        tmp = n.left
+        n.left = n.right
+        n.right = tmp
+
+        s.append(n.left)
+        s.append(n.right)
+
+    return root
+
+
+def invert_tree2(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    """
+    Time complexity: O(n)
+    Space complexity: O(n)
+    """
+    q = [root]
+    while q:
+        n = q.pop(0)
+        if n is None:
+            continue
+
+        # swap
+        tmp = n.left
+        n.left = n.right
+        n.right = tmp
+
+        q.append(n.left)
+        q.append(n.right)
 
     return root
 
 
 class TestInvertBinaryTree(unittest.TestCase):
 
-    def test_invert_binary_tree(self):
-        test_cases = [
+    @staticmethod
+    def parse_input():
+        data = [
             [[4, 2, 7, 1, 3, 6, 9], [4, 7, 2, 9, 6, 3, 1]],
             [[2, 1, 3], [2, 3, 1]],
             [[], []],
         ]
+        return data
 
-        for tc in test_cases:
-            h = tree_to_list(invert_tree(list_tree_to_tree(tc[0])))
+    def test_invert_binary_tree0(self):
+        data = self.parse_input()
+
+        for tc in data:
+            h = tree_to_list(invert_tree0(list_tree_to_tree(tc[0])))
+            self.assertEqual(h, tc[1])
+
+    def test_invert_binary_tree1(self):
+        data = self.parse_input()
+
+        for tc in data:
+            h = tree_to_list(invert_tree1(list_tree_to_tree(tc[0])))
+            self.assertEqual(h, tc[1])
+
+    def test_invert_binary_tree2(self):
+        data = self.parse_input()
+
+        for tc in data:
+            h = tree_to_list(invert_tree2(list_tree_to_tree(tc[0])))
             self.assertEqual(h, tc[1])
 
 
