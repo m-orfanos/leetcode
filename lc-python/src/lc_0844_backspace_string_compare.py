d5f5fe9f35ca2291740e931485bfd0bbbb624b04
Miltiadis Orfanos <morfanos@tuta.io>
2024-04-18 10:06:53 -0400

    0844 adds solution

--- lc-python/src/lc_0844_backspace_string_compare.py
+++ lc-python/src/lc_0844_backspace_string_compare.py
@@ -23,6 +23,46 @@ def backspace_string_compare_naive(s: str, t: str) -> bool:
     return ss == tt
 
 
+def backspace_string_compare(s: str, t: str) -> bool:
+    """
+    Time complexity: O(m+n)
+    Space complexity: O(1)
+    """
+
+    def next(s: str, i: int) -> int:
+        cnt = 0
+        while i >= 0:
+            if s[i] == "#":
+                i -= 1
+                cnt += 1
+                continue
+            if cnt > 0:
+                i -= 1
+                cnt -= 1
+                continue
+            return i
+        return -1
+
+    si = len(s) - 1
+    ti = len(t) - 1
+    while si >= 0 and ti >= 0:
+        si = next(s, si)
+        ti = next(t, ti)
+        if si < 0 or ti < 0:
+            break
+        if s[si] != t[ti]:
+            return False
+        si -= 1
+        ti -= 1
+
+    if si >= 0 and s[si] == "#":
+        si = next(s, si)
+    if ti >= 0 and t[ti] == "#":
+        ti = next(t, ti)
+
+    return si < 0 and ti < 0
+
+
 class TestBackspaceStringCompare(unittest.TestCase):
     @staticmethod
     def parse_input():
@@ -49,6 +89,17 @@ class TestBackspaceStringCompare(unittest.TestCase):
             actual = backspace_string_compare_naive(s, t)
 
             self.assertEqual(actual, expected)
+
+    def test_backspace_string_compare(self):
+        test_cases = self.parse_input()
+        for tc in test_cases:
+            s = tc[0]
+            t = tc[1]
+            expected = tc[2]
+
+            actual = backspace_string_compare(s, t)
+
+            self.assertEqual(actual, expected, f"{s} {t}")
 
 
 if __name__ == "__main__":
