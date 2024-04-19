5e7c3bbc36c719911926e173c39efbcb69dbfd86
Miltiadis Orfanos <morfanos@tuta.io>
2024-04-19 10:56:13 -0400

    0190 adds solution

--- lc-python/src/lc_0190_reverse_bits.py
+++ lc-python/src/lc_0190_reverse_bits.py
@@ -13,6 +13,17 @@ def reverse_bits(n: int) -> int:
     return rev
 
 
+def reverse_bits1(n: int) -> int:
+    # 00000010100101000001111010011100
+    #       10100101000001111010011100 = bin(43261596)
+    # note the missing leading zeros
+    rev = 0
+    for _ in range(32):
+        rev = 2 * rev + n % 2
+        n //= 2
+    return rev
+
+
 class TestReverseBits(unittest.TestCase):
     @staticmethod
     def parse_input():
@@ -31,6 +42,16 @@ class TestReverseBits(unittest.TestCase):
             actual = reverse_bits(a)
 
             self.assertEqual(actual, expected)
+
+    def test_reverse_bits1(self):
+        test_cases = self.parse_input()
+        for tc in test_cases:
+            a = tc[0]
+            expected = tc[1]
+
+            actual = reverse_bits1(a)
+
+            self.assertEqual(actual, expected, f"{a} {expected}")
 
 
 if __name__ == "__main__":
