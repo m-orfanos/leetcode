130deb0fab2625207b53a263435d47907dbb9ac3
Miltiadis Orfanos <morfanos@tuta.io>
2024-06-03 14:27:35 -0400

    solves 0001 two sum (typescript)

---
+++ lc-typescript/README.md
@@ -0,0 +1,32 @@
+# README
+
+Yet Another LeetCode solutions repository… in typescript.
+
+## Prerequisites
+
+install deno https://deno.com/
+
+```shell
+curl -fsSL https://deno.land/install.sh | sh
+```
+
+## Getting started
+
+Run unit tests via the helper script.
+
+```shell
+./run_tests.sh
+```
+
+sample output
+
+```shell
+running 1 test from ./src/lc_0001_two_sum.ts
+0001 Two Sum ...
+  Test case #0 ... ok (1ms)
+  Test case #1 ... ok (0ms)
+  Test case #2 ... ok (0ms)
+0001 Two Sum ... ok (0ms)
+
+ok | 1 passed (3 steps) | 0 failed (1ms)
+```
---
+++ lc-typescript/src/lc_0001_two_sum.ts
@@ -0,0 +1,33 @@
+import { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
+
+function two_sum(nums: number[], target: number) {
+  const nums_map: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums_map.has(nums[i])) {
+      return [nums_map.get(nums[i])!, i];
+    }
+    nums_map.set(target - nums[i], i);
+  }
+  return [];
+}
+
+Deno.test("0001 Two Sum", async (t) => {
+  const test_cases: [number[], number, number[]][] = [
+    [[2, 7, 11, 15], 9, [0, 1]],
+    [[3, 2, 4], 6, [1, 2]],
+    [[3, 3], 6, [0, 1]],
+  ];
+
+  for (let i = 0; i < test_cases.length; i += 1) {
+    await t.step(`Test case #${i}`, () => {
+      const test_case = test_cases[i];
+
+      const nums = test_case[0];
+      const target = test_case[1];
+      const expected = test_case[2];
+
+      const actual = two_sum(nums, target);
+      assertEquals(actual, expected);
+    });
+  }
+});
