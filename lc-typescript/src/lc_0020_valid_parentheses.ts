181ca14f4785339a0fd9f1843f1e1d5ca8363722
Miltiadis Orfanos <morfanos@tuta.io>
2024-06-04 01:32:23 -0400

    solves 0020 valid parentheses (typescript)

---
+++ lc-typescript/src/lc_0020_valid_parentheses.ts
@@ -0,0 +1,56 @@
+import { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
+
+function is_valid(s: string): boolean {
+  if (s.length % 2 !== 0) {
+    return false;
+  }
+
+  const open_brackets = "({[";
+  const matching_brackets: { [key: string]: string } = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  const stack: string[] = [];
+  for (let i = 0; i < s.length; i += 1) {
+    if (open_brackets.includes(s[i])) {
+      stack.push(s[i]);
+      continue;
+    }
+
+    if (stack.length === 0) {
+      return false;
+    }
+
+    const ch = stack.pop()!;
+    if (s[i] !== matching_brackets[ch]) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+}
+
+Deno.test("0020 Valid Parenthesis", async (t) => {
+  const test_cases: [string, boolean][] = [
+    ["()", true],
+    ["()[]{}", true],
+    ["(]", false],
+    ["[", false],
+    ["]", false],
+  ];
+
+  for (let i = 0; i < test_cases.length; i += 1) {
+    await t.step(`Test case #${i}`, () => {
+      const test_case = test_cases[i];
+
+      const str = test_case[0];
+      const expected = test_case[1];
+
+      const actual = is_valid(str);
+
+      assertEquals(actual, expected);
+    });
+  }
+});
