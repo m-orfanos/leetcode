4e2efa3e0a2fc4de9bd0620588b4936fdeb66ad5
Miltiadis Orfanos <morfanos@tuta.io>
2024-06-05 00:36:21 -0400

    solves 0226 invert binary tree (typescript)

---
+++ lc-typescript/src/lc_0226_invert_binary_tree.ts
@@ -0,0 +1,82 @@
+import { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
+
+import { TreeNode, array_to_tree, tree_to_array } from "./shared/utils.ts";
+
+function invert_tree0(root: TreeNode | null): TreeNode | null {
+    const stack = [root];
+    while (stack.length > 0) {
+        const n = stack.pop();
+        if (n == null) {
+            continue;
+        }
+
+        // swap
+        const t = n.left;
+        n.left = n.right;
+        n.right = t;
+
+        // traverse
+        stack.push(n.left);
+        stack.push(n.right);
+    }
+
+    return root;
+};
+
+function invert_tree1(root: TreeNode | null): TreeNode | null {
+    function dfs(node: TreeNode | null) {
+        if (node == null) {
+            return;
+        }
+
+        // swap
+        const t = node.left;
+        node.left = node.right;
+        node.right = t;
+
+        // traverse
+        dfs(node.left);
+        dfs(node.right);
+    }
+
+    dfs(root);
+
+    return root;
+}
+
+Deno.test("0226 Invert Binary Tree", async (t) => {
+    const test_cases = [
+        [[4, 2, 7, 1, 3, 6, 9], [4, 7, 2, 9, 6, 3, 1]],
+        [[2, 1, 3], [2, 3, 1]],
+        [[], []],
+    ];
+
+    await t.step(`Iterative DFS`, () => {
+        for (let i = 0; i < test_cases.length; i += 1) {
+            const test_case = test_cases[i];
+
+            const tree1 = array_to_tree(test_case[0]);
+            const tree3 = invert_tree0(tree1);
+
+            const actual = tree_to_array(tree3);
+            const expected = test_case[1];
+
+            assertEquals(actual, expected);
+        }
+    });
+
+    await t.step(`Recursive DFS`, () => {
+        for (let i = 0; i < test_cases.length; i += 1) {
+            const test_case = test_cases[i];
+
+            const tree1 = array_to_tree(test_case[0]);
+            const tree3 = invert_tree1(tree1);
+
+            const actual = tree_to_array(tree3);
+            const expected = test_case[1];
+
+            assertEquals(actual, expected);
+        }
+    });
+
+});
--- lc-typescript/src/shared/utils.ts
+++ lc-typescript/src/shared/utils.ts
@@ -30,3 +30,66 @@ export function linked_list_to_array(l: ListNode | null): number[] {
     }
     return arr;
 }
+
+export class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+export function array_to_tree(arr: number[]): TreeNode | null {
+    if (arr.length == 0) {
+        return null;
+    }
+
+    const t = new TreeNode(arr[0]);
+    if (arr.length === 1) {
+        return t;
+    }
+
+    const q = [t];
+    let i = 1;
+    while (q.length > 0 && i < arr.length) {
+        const n = q.shift();
+        if (n == null) {
+            continue;
+        }
+
+        n.left = new TreeNode(arr[i]);
+        n.right = new TreeNode(arr[i + 1]);
+
+        q.push(n.left);
+        q.push(n.right);
+
+        i += 2;
+    }
+
+    return t;
+}
+
+export function tree_to_array(tree: TreeNode | null): number[] {
+    if (tree == null) {
+        return [];
+    }
+
+    const arr = [];
+    const q: [TreeNode | null] = [tree];
+    while (q.length > 0) {
+        const n = q.shift();
+        if (n == null) {
+            continue;
+        }
+
+        arr.push(n.val);
+
+        q.push(n.left);
+        q.push(n.right);
+    }
+
+    return arr;
+}
