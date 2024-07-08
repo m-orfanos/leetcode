e166300a7b890497a7b520a56d87349c6902d26d
Miltiadis Orfanos <morfanos@tuta.io>
2024-07-08 17:00:27 -0400

    solves 0155 min stack (python)

---
+++ lc-python/src/lc_0155_min_stack.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if self.stack:
+            min_val = self.stack[-1][1]
+        else:
+            min_val = val
+        min_val = min(val, min_val)
+
+        self.stack.append((val, min_val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+
+class Test(unittest.TestCase):
+    def test_(self):
+        s = MinStack()
+        s.push(-2)
+        s.push(0)
+        s.push(-3)
+
+        self.assertEqual(s.getMin(), -3)
+        s.pop()
+        self.assertEqual(s.top(), 0)
+        self.assertEqual(s.getMin(), -2)
+
+
+if __name__ == "__main__":
+    unittest.main()
