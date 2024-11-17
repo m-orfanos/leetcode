db07bf8509a1df36b056562158c1646a5254cdf5
Miltiadis Orfanos <morfanos@tuta.io>
2024-11-17 14:17:31 -0500

    solves 1683 invalid tweets (sql)

---
+++ lc-sql/lc_1683_invalid_tweets.sql
@@ -0,0 +1,12 @@
+SET client_min_messages TO WARNING;
+
+-- SCHEMA
+Create table If Not Exists Tweets(tweet_id int, content varchar(50));
+Truncate table Tweets;
+insert into Tweets (tweet_id, content) values ('1', 'Let us Code');
+insert into Tweets (tweet_id, content) values ('2', 'More than fifteen chars are here!');
+
+-- SOLUTION
+SELECT tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15;
