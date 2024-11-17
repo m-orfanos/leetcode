6831d895fe6763f2739d0183691f13025e17ea2f
Miltiadis Orfanos <morfanos@tuta.io>
2024-11-17 12:06:12 -0500

    0110 adds additional approach

--- lc-typescript/src/lc_0110_balanced_binary_tree.ts
+++ lc-typescript/src/lc_0110_balanced_binary_tree.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
-import { TreeNode, array_to_tree } from "./shared/utils.ts";
+import { array_to_tree, TreeNode } from "./shared/utils.ts";
 
-function is_balanced(root: TreeNode | null): boolean {
+function is_balanced1(root: TreeNode | null): boolean {
     function dfs(node: TreeNode | null): number {
         if (node == null) {
             return 0;
@@ -26,7 +26,24 @@ function is_balanced(root: TreeNode | null): boolean {
     }
 
     return dfs(root) != -1;
-};
+}
+
+function is_balanced2(root: TreeNode | null): boolean {
+    function dfs(node: TreeNode | null): number {
+        if (!node) {
+            return 0;
+        }
+        const lhs = dfs(node.left);
+        const rhs = dfs(node.right);
+        const diff = Math.abs(lhs - rhs);
+        if (lhs < 0 || rhs < 0 || diff > 1) {
+            return -1;
+        }
+        return 1 + Math.max(lhs, rhs);
+    }
+
+    return dfs(root) !== -1;
+}
 
 Deno.test("0110 Balanced Binary Tree", () => {
     const test_cases: [(number | null)[], boolean][] = [
@@ -41,8 +58,11 @@ Deno.test("0110 Balanced Binary Tree", () => {
         const root = array_to_tree(test_case[0]);
 
         const expected = test_case[1];
-        const actual = is_balanced(root);
 
-        assertEquals(actual, expected);
+        const actual1 = is_balanced1(root);
+        assertEquals(actual1, expected);
+
+        const actual2 = is_balanced2(root);
+        assertEquals(actual2, expected);
     }
 });
