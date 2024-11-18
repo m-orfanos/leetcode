1975c714b0be980854be929df74b20aaa470de53
Miltiadis Orfanos <morfanos@tuta.io>
2024-11-17 21:46:03 -0500

    solves 1280 students and exams (sql)

---
+++ lc-sql/lc_1280_students_and_exams.sql
@@ -0,0 +1,44 @@
+SET client_min_messages TO WARNING;
+
+-- SCHEMA
+Create table If Not Exists Students (student_id int, student_name varchar(20));
+Create table If Not Exists Subjects (subject_name varchar(20));
+Create table If Not Exists Examinations (student_id int, subject_name varchar(20));
+
+Truncate table Students;
+insert into Students (student_id, student_name) values ('1', 'Alice');
+insert into Students (student_id, student_name) values ('2', 'Bob');
+insert into Students (student_id, student_name) values ('13', 'John');
+insert into Students (student_id, student_name) values ('6', 'Alex');
+
+Truncate table Subjects;
+insert into Subjects (subject_name) values ('Math');
+insert into Subjects (subject_name) values ('Physics');
+insert into Subjects (subject_name) values ('Programming');
+
+Truncate table Examinations;
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+insert into Examinations (student_id, subject_name) values ('1', 'Physics');
+insert into Examinations (student_id, subject_name) values ('1', 'Programming');
+insert into Examinations (student_id, subject_name) values ('2', 'Programming');
+insert into Examinations (student_id, subject_name) values ('1', 'Physics');
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+insert into Examinations (student_id, subject_name) values ('13', 'Math');
+insert into Examinations (student_id, subject_name) values ('13', 'Programming');
+insert into Examinations (student_id, subject_name) values ('13', 'Physics');
+insert into Examinations (student_id, subject_name) values ('2', 'Math');
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+
+-- SOLUTION 
+SELECT
+    st.student_id, 
+    st.student_name, 
+    su.subject_name, 
+    COUNT(e.subject_name) as attended_exams
+FROM students st
+CROSS JOIN subjects su
+LEFT OUTER JOIN examinations e ON
+    st.student_id = e.student_id AND
+    su.subject_name = e.subject_name
+GROUP BY st.student_id, st.student_name, su.subject_name
+ORDER BY st.student_id, su.subject_name;
