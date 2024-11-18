6e8ff3c9aa1c441edf21c844ac00309c57837662
Miltiadis Orfanos <morfanos@tuta.io>
2024-11-17 20:50:53 -0500

    solves 0197 rising temperature (sql)

---
+++ lc-sql/lc_0197_rising_temperature.sql
@@ -0,0 +1,15 @@
+SET client_min_messages TO WARNING;
+
+-- SCHEMA
+Create table If Not Exists Weather (id int, recordDate date, temperature int);
+Truncate table Weather;
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10');
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25');
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20');
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30');
+
+-- SOLUTION
+SELECT w2.id
+FROM Weather w1
+CROSS JOIN Weather w2
+WHERE w2.recorddate - w1.recorddate = 1 AND w2.temperature > w1.temperature;
