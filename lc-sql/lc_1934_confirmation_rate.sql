6224ff32db2d6ad193e49ce00bb88c262a37b71d
Miltiadis Orfanos <morfanos@tuta.io>
2024-11-23 16:26:42 -0500

    1934 cleanup

--- lc-sql/lc_1934_confirmation_rate.sql
+++ lc-sql/lc_1934_confirmation_rate.sql
@@ -1,24 +1,24 @@
--- SET client_min_messages TO WARNING;
+SET client_min_messages TO WARNING;
 
--- -- SCHEMA
--- CREATE TYPE confirmation_status AS ENUM('confirmed','timeout');
--- Create table If Not Exists Signups (user_id int, time_stamp TIMESTAMP);
--- Create table If Not Exists Confirmations (user_id int, time_stamp TIMESTAMP, action confirmation_status);
+-- SCHEMA
+CREATE TYPE confirmation_status AS ENUM('confirmed','timeout');
+Create table If Not Exists Signups (user_id int, time_stamp TIMESTAMP);
+Create table If Not Exists Confirmations (user_id int, time_stamp TIMESTAMP, action confirmation_status);
 
--- Truncate table Signups;
--- insert into Signups (user_id, time_stamp) values ('3', '2020-03-21 10:16:13');
--- insert into Signups (user_id, time_stamp) values ('7', '2020-01-04 13:57:59');
--- insert into Signups (user_id, time_stamp) values ('2', '2020-07-29 23:09:44');
--- insert into Signups (user_id, time_stamp) values ('6', '2020-12-09 10:39:37');
+Truncate table Signups;
+insert into Signups (user_id, time_stamp) values ('3', '2020-03-21 10:16:13');
+insert into Signups (user_id, time_stamp) values ('7', '2020-01-04 13:57:59');
+insert into Signups (user_id, time_stamp) values ('2', '2020-07-29 23:09:44');
+insert into Signups (user_id, time_stamp) values ('6', '2020-12-09 10:39:37');
 
--- Truncate table Confirmations;
--- insert into Confirmations (user_id, time_stamp, action) values ('3', '2021-01-06 03:30:46', 'timeout');
--- insert into Confirmations (user_id, time_stamp, action) values ('3', '2021-07-14 14:00:00', 'timeout');
--- insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-12 11:57:29', 'confirmed');
--- insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-13 12:58:28', 'confirmed');
--- insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-14 13:59:27', 'confirmed');
--- insert into Confirmations (user_id, time_stamp, action) values ('2', '2021-01-22 00:00:00', 'confirmed');
--- insert into Confirmations (user_id, time_stamp, action) values ('2', '2021-02-28 23:59:59', 'timeout');
+Truncate table Confirmations;
+insert into Confirmations (user_id, time_stamp, action) values ('3', '2021-01-06 03:30:46', 'timeout');
+insert into Confirmations (user_id, time_stamp, action) values ('3', '2021-07-14 14:00:00', 'timeout');
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-12 11:57:29', 'confirmed');
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-13 12:58:28', 'confirmed');
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-14 13:59:27', 'confirmed');
+insert into Confirmations (user_id, time_stamp, action) values ('2', '2021-01-22 00:00:00', 'confirmed');
+insert into Confirmations (user_id, time_stamp, action) values ('2', '2021-02-28 23:59:59', 'timeout');
 
 -- SOLUTION 1
 SELECT
