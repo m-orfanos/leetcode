82dabe1573e2e036ab4181b95ab6dde3b16507d2
Miltiadis Orfanos <morfanos@tuta.io>
2024-11-24 13:36:58 -0500

    0383 adds additional approach

--- lc-typescript/src/lc_0383_ransom_note.ts
+++ lc-typescript/src/lc_0383_ransom_note.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
 
-function can_construct(ransomNote: string, magazine: string): boolean {
+function can_construct1(ransomNote: string, magazine: string): boolean {
     if (ransomNote.length > magazine.length) {
         return false;
     }
@@ -24,6 +24,31 @@ function can_construct(ransomNote: string, magazine: string): boolean {
     return true;
 };
 
+function can_construct2(ransomNote: string, magazine: string): boolean {
+    // const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    // alphabet.split("").forEach((l) => console.log(l.charCodeAt(0)));
+    // 97, 98, 99, ...,
+  
+    if (magazine.length < ransomNote.length) {
+      return false;
+    }
+  
+    const histogram: number[] = [];
+    for (let i = 0; i < 26; i++) {
+      histogram.push(0);
+    }
+  
+    for (let i = 0; i < magazine.length; i++) {
+      histogram[magazine.charCodeAt(i) - 97] += 1;
+    }
+  
+    for (let j = 0; j < ransomNote.length; j++) {
+      histogram[ransomNote.charCodeAt(j) - 97] -= 1;
+    }
+  
+    return histogram.every((l) => l >= 0);
+  }
+
 Deno.test("0383 Random Note", () => {
     const test_cases: [string, string, boolean][] = [
         ["a", "b", false],
@@ -38,8 +63,11 @@ Deno.test("0383 Random Note", () => {
         const magazine = test_case[1];
 
         const expected = test_case[2];
-        const actual = can_construct(ransomNote, magazine);
 
-        assertEquals(actual, expected);
+        const actual1 = can_construct1(ransomNote, magazine);
+        assertEquals(actual1, expected);
+
+        const actual2 = can_construct2(ransomNote, magazine);
+        assertEquals(actual2, expected);
     }
 });
