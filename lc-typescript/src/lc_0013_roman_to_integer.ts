9788f727eb9f7f222770d2ec74e78d8ec3ad7f98
Miltiadis Orfanos <morfanos@tuta.io>
2024-11-25 00:07:12 -0500

    0013 adds additional approach

--- lc-typescript/src/lc_0013_roman_to_integer.ts
+++ lc-typescript/src/lc_0013_roman_to_integer.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
 
-function roman_to_integer(s: string): number {
+function roman_to_integer1(s: string): number {
     const symbols: { [key: string]: number } = {
         "I": 1,
         "V": 5,
@@ -30,6 +30,28 @@ function roman_to_integer(s: string): number {
     return ans;
 }
 
+function roman_to_integer2(s: string): number {
+    const mx: { [key: string]: number } = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    };
+
+    let ans = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        const coef = mx[s[i]] < mx[s[i + 1]] ? -1 : 1;
+        ans += coef * mx[s[i]];
+    }
+
+    ans += mx[s[s.length - 1]];
+
+    return ans;
+}
+
 Deno.test("0013 Roman to Integer", () => {
     const test_cases: [string, number][] = [
         ["III", 3],
@@ -43,7 +65,10 @@ Deno.test("0013 Roman to Integer", () => {
         const s = test_case[0];
         const expected = test_case[1];
 
-        const actual = roman_to_integer(s);
-        assertEquals(actual, expected);
+        const actual1 = roman_to_integer1(s);
+        assertEquals(actual1, expected);
+
+        const actual2 = roman_to_integer2(s);
+        assertEquals(actual2, expected);
     }
 });
