34e062979a1d82c72d31184d55e134e131207f00
Miltiadis Orfanos <morfanos@tuta.io>
2024-11-25 01:25:24 -0500

    0338 adds additional approach

--- lc-typescript/src/lc_0338_counting_bits.ts
+++ lc-typescript/src/lc_0338_counting_bits.ts
@@ -26,6 +26,39 @@ function count_bits2(n: number): number[] {
     return ans;
 }
 
+function count_bits3(n: number): number[] {
+    // dp[0]  =    0
+    //
+    // dp[1]  =    1 = dp[0] + 1
+    //
+    // dp[2]  =   10 = dp[1] + 0
+    // dp[3]  =   11 = dp[1] + 1
+    //
+    // dp[4]  =  100 = dp[2] + 0
+    // dp[5]  =  101 = dp[2] + 1
+    //
+    // dp[6]  =  110 = dp[3] + 0
+    // dp[7]  =  111 = dp[3] + 1
+    //
+    // dp[8]  = 1000 = dp[4] + 0
+    // dp[9]  = 1001 = dp[4] + 1
+    //
+    // dp[10] = 1010 = dp[5] + 0
+    // dp[11] = 1011 = dp[5] + 1
+    //
+    // dp[12] = 1100 = dp[6] + 0
+    // dp[13] = 1101 = dp[6] + 1
+    //
+    // dp[14] = 1110 = dp[7] + 0
+    // dp[15] = 1111 = dp[7] + 1
+    // ...
+    const dp = [0];
+    for (let i = 1; i <= n; i++) {
+        dp.push(dp[Math.floor(i / 2)] + i % 2);
+    }
+    return dp;
+}
+
 Deno.test("0038 Counting Bits", () => {
     const test_cases: [number, number[]][] = [
         [0, [0]],
@@ -47,8 +80,10 @@ Deno.test("0038 Counting Bits", () => {
 
         const actual1 = count_bits1(n);
         const actual2 = count_bits2(n);
+        const actual3 = count_bits3(n);
 
         assertEquals(actual1, expected, `failed for ${n}`);
         assertEquals(actual2, expected, `failed for ${n}`);
+        assertEquals(actual3, expected, `failed for ${n}`);
     }
 });
