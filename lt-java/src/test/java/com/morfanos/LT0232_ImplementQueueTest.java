67e3cf874ca7e9193cc44be850b2996e6ce45e38
Miltiadis Orfanos <morfanos@tuta.io>
2025-01-05 17:50:30 -0500

    solves 0232 implement queue using stacks (java)

---
+++ lt-java/src/test/java/com/morfanos/LT0232_ImplementQueueTest.java
@@ -0,0 +1,21 @@
+package com.morfanos;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+class LT0232_ImplementQueueTest {
+
+    @Test
+    void test1() {
+        var q = new LT0232_ImplementQueue.MyQueue();
+        q.push(1);
+        q.push(2);
+
+        assertEquals(1, q.peek());
+        assertEquals(1, q.pop());
+        assertFalse(q.empty());
+    }
+
+}
