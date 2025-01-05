04797caf953ff50cc61a2babc09d0ff1122f078f
Miltiadis Orfanos <morfanos@tuta.io>
2025-01-05 17:26:16 -0500

    solves 0110 balanced binary tree (java)

---
+++ lt-java/src/test/java/com/morfanos/LT0110_BalancedBinaryTreeTest.java
@@ -0,0 +1,38 @@
+package com.morfanos;
+
+import static com.morfanos.LT0110_BalancedBinaryTree.isBalanced;
+import static com.morfanos.shared.Helper.toTreeNode;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class LT0110_BalancedBinaryTreeTest {
+
+    @Test
+    void isBalanced1() {
+        var l = Arrays.asList(3, 9, 20, null, null, 15, 7);
+        var t = toTreeNode(l);
+        var actual = isBalanced(t);
+        assertTrue(actual);
+    }
+
+    @Test
+    void isBalanced2() {
+        var l = Arrays.asList(1, 2, 2, 3, 3, null, null, 4, 4);
+        var t = toTreeNode(l);
+        var actual = isBalanced(t);
+        assertFalse(actual);
+    }
+
+    @Test
+    void isBalanced3() {
+        var l = Arrays.asList(1, 2, 2, 3, null, null, 3, 4, null, null, null, null, null, null, 4);
+        var t = toTreeNode(l);
+        var actual = isBalanced(t);
+        assertFalse(actual);
+    }
+
+}
