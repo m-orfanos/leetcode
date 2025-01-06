af170f68cd785c70a42a1ff30eef4cf184395533
Miltiadis Orfanos <morfanos@tuta.io>
2025-01-06 13:27:00 -0500

    solves 0100 same tree (java)

---
+++ lt-java/src/test/java/com/morfanos/LT0100_SameTreeTest.java
@@ -0,0 +1,30 @@
+package com.morfanos;
+
+import static com.morfanos.LT0100_SameTree.isSameTree;
+import static com.morfanos.shared.Helper.toTreeNode;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class LT0100_SameTreeTest {
+
+    @Test
+    void isSameTree1() {
+        var t1 = toTreeNode(Arrays.asList(1, 2, 3));
+        var t2 = toTreeNode(Arrays.asList(1, 2, 3));
+        var actual = isSameTree(t1, t2);
+        assertTrue(actual);
+    }
+
+    @Test
+    void isSameTree2() {
+        var t1 = toTreeNode(Arrays.asList(1, 2));
+        var t2 = toTreeNode(Arrays.asList(1, null, 2));
+        var actual = isSameTree(t1, t2);
+        assertFalse(actual);
+    }
+
+}
