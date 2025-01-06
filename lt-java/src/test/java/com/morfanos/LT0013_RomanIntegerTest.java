30cd018508270ba6cc1830f689d5d136cbfb4fd1
Miltiadis Orfanos <morfanos@tuta.io>
2025-01-06 12:37:43 -0500

    solves 0013 roman to integer

---
+++ lt-java/src/test/java/com/morfanos/LT0013_RomanIntegerTest.java
@@ -0,0 +1,22 @@
+package com.morfanos;
+
+import static com.morfanos.LT0013_RomanInteger.romanToInt;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class LT0013_RomanIntegerTest {
+
+    @Test
+    void romanToInt1() {
+        var actual = romanToInt("III");
+        assertEquals(3, actual);
+    }
+
+    @Test
+    void romanToInt2() {
+        var actual = romanToInt("MCMXCIV");
+        assertEquals(1994, actual);
+    }
+
+}
