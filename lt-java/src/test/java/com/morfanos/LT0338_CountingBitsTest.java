7ea2a1315115af36d12aaf9065eff7360f0d143c
Miltiadis Orfanos <morfanos@tuta.io>
2025-01-06 13:11:02 -0500

    solves 0338 counting bits (java)

---
+++ lt-java/src/test/java/com/morfanos/LT0338_CountingBitsTest.java
@@ -0,0 +1,29 @@
+package com.morfanos;
+
+import static com.morfanos.LT0338_CountingBits.countBits;
+import static com.morfanos.shared.Helper.toArray;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class LT0338_CountingBitsTest {
+
+    @Test
+    void countBits1() {
+        var actual = countBits(2);
+        assertArrayEquals(toArray(0, 1, 1), actual);
+    }
+
+    @Test
+    void countBits2() {
+        var actual = countBits(5);
+        assertArrayEquals(toArray(0, 1, 1, 2, 1, 2), actual);
+    }
+
+    @Test
+    void countBits3() {
+        var actual = countBits(8);
+        assertArrayEquals(toArray(0, 1, 1, 2, 1, 2, 2, 3, 1), actual);
+    }
+
+}
