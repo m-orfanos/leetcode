367b57ec064f358ab2cb652b939882516d16fdaf
Miltiadis Orfanos <morfanos@tuta.io>
2025-01-06 12:09:29 -0500

    solves 0543 diameter binary tree (java)

---
+++ lt-java/src/test/java/com/morfanos/LT0543_DiameterBinaryTreeTest.java
@@ -0,0 +1,20 @@
+package com.morfanos;
+
+import static com.morfanos.shared.Helper.toTreeNode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class LT0543_DiameterBinaryTreeTest {
+
+    @Test
+    void diameterOfBinaryTree1() {
+        var a = List.of(1, 2, 3, 4, 5);
+        var t = toTreeNode(a);
+        var actual = LT0543_DiameterBinaryTree.diameterOfBinaryTree(t);
+        assertEquals(3, actual);
+    }
+
+}
