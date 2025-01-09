3a50e9a9c9ef35d5d6767d2037a3e8a656ac7377
Miltiadis Orfanos <morfanos@tuta.io>
2025-01-09 01:23:14 -0500

    solves 0015 three sum (java)

---
+++ lt-java/src/main/java/com/morfanos/LT0015ThreeSum.java
@@ -0,0 +1,111 @@
+package com.morfanos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class LT0015ThreeSum {
+
+    static List<List<Integer>> threeSum1(int[] nums) {
+        // divide-and-conquer approach
+
+        var pos = new HashMap<Integer, Integer>();
+        var neg = new HashMap<Integer, Integer>();
+        var zeroes = 0;
+        for (var n : nums) {
+            if (n > 0) {
+                pos.put(n, 1 + pos.getOrDefault(n, 0));
+            } else if (n < 0) {
+                neg.put(n, 1 + neg.getOrDefault(n, 0));
+            } else {
+                zeroes += 1;
+            }
+        }
+
+        var triples = new ArrayList<List<Integer>>();
+        if (zeroes > 0) {
+            // case (-p,0,p)
+            for (var p : pos.keySet()) {
+                if (neg.containsKey(-p)) {
+                    triples.add(List.of(-p, 0, p));
+                }
+            }
+            // case (0,0,0)
+            if (zeroes > 2) {
+                triples.add(List.of(0, 0, 0));
+            }
+        }
+
+        // case (n1,n2,p)
+        for (var n1 : neg.keySet()) {
+            for (var n2 : neg.keySet()) {
+                if (n1 > n2 || (n1 == n2 && neg.get(n1) < 2)) {
+                    continue;
+                }
+                if (pos.containsKey(-(n1 + n2))) {
+                    triples.add(List.of(n1, n2, -(n1 + n2)));
+                }
+            }
+
+        }
+
+        // case (n,p1,p2)
+        for (var p1 : pos.keySet()) {
+            for (var p2 : pos.keySet()) {
+                if (p1 > p2 || (p1 == p2 && pos.get(p1) < 2)) {
+                    continue;
+                }
+                if (neg.containsKey(-(p1 + p2))) {
+                    triples.add(List.of(-(p1 + p2), p1, p2));
+                }
+            }
+        }
+
+        return triples;
+    }
+
+    static List<List<Integer>> threeSum2(int[] nums) {
+        // two-pointer approach
+
+        Arrays.sort(nums);
+
+        var triples = new ArrayList<List<Integer>>();
+        var prev = Integer.MIN_VALUE;
+        for (var i = 0; i < nums.length; i++) {
+            // skip duplicates
+            if (nums[i] == prev) {
+                continue;
+            }
+            prev = nums[i];
+
+            var target = -nums[i];
+
+            var l = i + 1;
+            var r = nums.length - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] < target) {
+                    l += 1;
+                } else if (nums[l] + nums[r] > target) {
+                    r -= 1;
+                } else {
+                    triples.add(List.of(nums[i], nums[l], nums[r]));
+
+                    l += 1;
+                    r -= 1;
+
+                    // skip duplicates
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l += 1;
+                    }
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r -= 1;
+                    }
+                }
+            }
+        }
+
+        return triples;
+    }
+
+}
---
+++ lt-java/src/test/java/com/morfanos/LT0015ThreeSumTest.java
@@ -0,0 +1,83 @@
+package com.morfanos;
+
+import static com.morfanos.LT0015ThreeSum.threeSum1;
+import static com.morfanos.LT0015ThreeSum.threeSum2;
+import static com.morfanos.shared.Helper.to1DArray;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class LT0015ThreeSumTest {
+
+    @Test
+    void threeSum11() {
+        var actual = threeSum1(to1DArray(-1, 0, 1, 2, -1, -4));
+        var expected = List.of(
+                List.of(-1, 0, 1),
+                List.of(-1, -1, 2));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void threeSum12() {
+        var actual = threeSum1(to1DArray(0, 1, 1));
+        var expected = List.of();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void threeSum13() {
+        var actual = threeSum1(to1DArray(0, 0, 0));
+        var expected = List.of(List.of(0, 0, 0));
+        assertEquals(expected, actual);
+    }
+
+    private Comparator<? super List<Integer>> asc() {
+        return (a, b) -> {
+            if (a.get(0) == b.get(0)) {
+                return a.get(1) - b.get(1);
+            } else {
+                return a.get(0) - b.get(0);
+            }
+        };
+    }
+
+    @Test
+    void threeSum14() {
+        var actual = threeSum1(to1DArray(-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4));
+        actual.sort(asc());
+
+        var expected = List.of(
+                List.of(-4, 0, 4),
+                List.of(-4, 1, 3),
+                List.of(-3, -1, 4),
+                List.of(-3, 0, 3),
+                List.of(-3, 1, 2),
+                List.of(-2, -1, 3),
+                List.of(-2, 0, 2),
+                List.of(-1, -1, 2),
+                List.of(-1, 0, 1));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void threeSum24() {
+        var actual = threeSum2(to1DArray(-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4));
+        var expected = List.of(
+                List.of(-4, 0, 4),
+                List.of(-4, 1, 3),
+                List.of(-3, -1, 4),
+                List.of(-3, 0, 3),
+                List.of(-3, 1, 2),
+                List.of(-2, -1, 3),
+                List.of(-2, 0, 2),
+                List.of(-1, -1, 2),
+                List.of(-1, 0, 1));
+        assertEquals(expected, actual);
+    }
+
+}
